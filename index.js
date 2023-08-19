d78f314cd93e5ebd95237daf0b63159e33765c42
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-19 01:42:34 +0000

    Dynamically size the overall grid to always be 1:1

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ for (i = 0; i < 256; i++) {
    const elementSquareDiv = document.createElement('div');
 
    elementSquareDiv.classList.add('individual-square-div');
-   elementSquareDiv.style.flexBasis = `${100/16}%`;
-   elementSquareDiv.style.height = elementSquareDiv.style.width;
 
    elementSquareDiv.textContent = i;
    
@@ -16,3 +14,27 @@ for (i = 0; i < 256; i++) {
 }
 
 elementBody.appendChild(elementSquareDivsContainer);
+
+function checkViewportSize() {
+   const allIndividualSquareDivs = document.querySelectorAll('#square-divs-container > .individual-square-div');
+   const squareDivsContainer = document.querySelector('#square-divs-container');
+   
+   if (window.innerWidth < window.innerHeight) {
+      console.log('width is smaller');
+      allIndividualSquareDivs.forEach((individualSquareDiv) => {
+         individualSquareDiv.style.flexBasis = `${100/16}%`;
+         individualSquareDiv.style.height = ``;
+         squareDivsContainer.style.maxWidth = ``;
+      });
+   } else {
+      console.log('height is smaller');
+      allIndividualSquareDivs.forEach((individualSquareDiv) => {
+         individualSquareDiv.style.flexBasis = `0`;
+         individualSquareDiv.style.height = `${100/16}%`;
+         squareDivsContainer.style.maxWidth = `${(individualSquareDiv.offsetHeight + 1) * 16}px`;
+      });
+   }
+}
+
+checkViewportSize();
+window.addEventListener('resize', checkViewportSize);
