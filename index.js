86d62ce887c796ebc8823447efc46be6e2a9f9e6
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-20 18:10:15 +0000

    Configure layout

---
+++ index.js
@@ -0,0 +1,12 @@
+const theGrid = document.getElementById('theGrid');
+
+let gridRowLength = 16;
+let gridColumnLength = 16;
+
+drawGrid();
+
+function drawGrid() {
+   for(let i = 0; i < (gridRowLength * gridColumnLength); i++) {
+      
+   }
+}
