f384e3076649ea2a44f2805da15f8d9c4884d373
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-20 18:30:32 +0000

    Fix grid container resizing logic

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const theGrid = document.getElementById('theGrid');
+const theGridContainer = document.getElementById('theGridContainer');
+const theGridItself = document.getElementById('theGridItself');
 
 let gridRowLength = 16;
 let gridColumnLength = 16;
 
+resizeTheGrid();
+window.addEventListener('resize', resizeTheGrid);
+
 drawGrid();
+
+function resizeTheGrid() {
+   theGridItself.style.height = `${0}px`;
+   theGridItself.style.width = `${0}px`;
+
+   gridContainerHeight = theGridContainer.clientHeight;
+   gridContainerWidth = theGridContainer.clientWidth;
+
+   if(gridContainerHeight < gridContainerWidth) {
+      theGridItself.style.height = `${gridContainerHeight}px`;
+      theGridItself.style.width = `${gridContainerHeight}px`;
+      
+      // DEBUG
+      theGridItself.textContent = 'height';
+      console.log('height');
+   } else {
+      theGridItself.style.height = `${gridContainerWidth}px`;
+      theGridItself.style.width = `${gridContainerWidth}px`;
+      
+      // DEBUG
+      theGridItself.textContent = 'width';
+      console.log('width');
+   }
+
+   console.log(`${gridContainerHeight} | ${gridContainerWidth}`)
+
+   return;
+}
 
 function drawGrid() {
    for(let i = 0; i < (gridRowLength * gridColumnLength); i++) {
-      
+
    }
 }
